baf158ab06e88c41d20c75a9cb1eda77b353de7f
Xaviierkasvar <javier_castillo_15@hotmail.es>
2021-11-21 17:19:55 -0500

    first commit

--- Controlador/AddCandidato.php
+++ Controlador/AddCandidato.php
@@ -5,7 +5,7 @@ require "../Modelo/conexionBasesDatos.php";
 require "../Modelo/Candidato.php";
 $cedula = $_POST['cedula'];
 $obj=new Candidato();
-$obj->CrearCandidato($cedula,$_POST['nombre'],$_POST['apellido'],$_POST['correo'],$_POST['rol'],$_POST['fecPost'],$_POST['salario'],$_POST['com']);
+$obj->CrearCandidato($_POST['cedula'],$_POST['nombre'],$_POST['apellido'],$_POST['correo'],$_POST['rol'],$_POST['fecPost'],$_POST['salario'],$_POST['com']);
 $resultado=$obj->AgregarCandidato();
 if ($resultado) 
 	header("location:NotificacionEtapa1.php?cedula=$cedula");
